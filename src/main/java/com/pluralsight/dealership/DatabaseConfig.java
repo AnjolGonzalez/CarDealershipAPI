4f9fb283fd6943446401e1bb642a16204de5d678
Angel Gonzalez <agonzalez000@appdev.yearup.org>
2023-12-22 10:50:39 -0500

    first commit

---
+++ src/main/java/com/pluralsight/dealership/DatabaseConfig.java
@@ -0,0 +1,27 @@
+package com.pluralsight.dealership;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
+public class DatabaseConfig {
+    private BasicDataSource basicDataSource;
+
+    @Bean
+    public DataSource dataSource() {
+        return basicDataSource;
+    }
+
+    @Autowired
+    public DatabaseConfig(@Value("${datasource.url}")String url,
+                          @Value("${datasource.username}")String username,
+                          @Value("${datasource.password}")String password) {
+        basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(url);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+    }
+}
